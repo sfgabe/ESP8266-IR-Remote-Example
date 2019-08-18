854885fcb675db6f65d3f136324b7d9e3ffe60c4
Gabe <36393392+sfgabe@users.noreply.github.com>
2019-08-18 01:49:19 -0400

    Update BasicWebRemote.ino

--- BasicWebRemote/BasicWebRemote.ino
+++ BasicWebRemote/BasicWebRemote.ino
@@ -1,4 +1,10 @@
 // Tested to work with Arduino IDE 1.9.5 and IRremoteESP8266 2.5.4-2.5.5 (25aa772)
+// Working hardware is Infrared Remote Decoder and reciever at https://www.ebay.com/itm/5V-IR-Infrared-Remote-Decoder-Encoding-Transmitter-Receiver-Wireless-Module/401092840926?ssPageName=STRK%3AMEBIDX%3AIT&_trksid=p2057872.m2749.l2649
+// and 50W+50W Portable Bluetooth Audio Receiver Amplifier Board TF Card Decoder with Remote Control DC 8-26V at https://amzn.to/2ZbyvtU
+// Adapted from https://github.com/ultrakiller2/ESP8266-IR-Remote-Example/blob/master/BasicWebRemote/BasicWebRemote.ino
+// Works with YS-IRTM module and WEMOS D1 mini with tx and rx crossed
+//
+//
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -8,14 +14,14 @@
 #include <IRremoteESP8266.h>
 #include <IRsend.h> // small change to compile on IRremoteESP8266 2.0+
 
-#define IR_SEND_PIN D2
+#define IR_SEND_PIN TX
 
 #define DELAY_BETWEEN_COMMANDS 1000
 
 IRsend irsend(IR_SEND_PIN);
 
-const char* ssid = "SSID";
-const char* password = "password";
+const char* ssid = "RollinsRogue";
+const char* password = "SonnyNights";
 
 ESP8266WebServer server(80);
 
@@ -46,17 +52,23 @@ void handleRoot() {
   website = website + "    <div class=\"container-fluid\">\n";
   // ------------------------- Power Controls --------------------------
   website = website + rowDiv;
-  website = website + generateButton("col-xs-4", "tvpower","TV Power", "tvpower");
-  website = website + generateButton("col-xs-4", "sspower","SS Power", "sspower");
-  website = website + generateButton("col-xs-4", "satpower","Sat Power", "satpower");
+  website = website + generateButton("col-xs-3", "power","Power", "power");
+  website = website + generateButton("col-xs-3", "play","Play", "play");
+  website = website + generateButton("col-xs-3", "stop","Stop", "stop");
+  website = website + generateButton("col-xs-3", "mode","Mode", "mode");
   website = website + endDiv;
   // ------------------------- Channel Controls --------------------------
   website = website + rowDiv;
-  website = website + generateButton("col-xs-3", "sschannel1","PS4", "sschannel1");
-  website = website + generateButton("col-xs-3", "sschannel2","FTV", "sschannel2");
-  //website = website + generateButton("col-xs-3", "sschannel3","---", "sschannel3");
-  website = website + generateButton("col-xs-3", "sschannel4","SAT", "sschannel4");
-  website = website + generateButton("col-xs-3", "sstvsound","TVS", "sstvsound");
+  website = website + generateButton("col-xs-4", "mute","Mute", "mute");
+  website = website + generateButton("col-xs-4", "back","Back", "back");
+  website = website + generateButton("col-xs-4", "eq","EQ", "eq");
+  website = website + endDiv;
+  // ------------------------- Direction Controls --------------------------
+  website = website + rowDiv;
+  website = website + generateButton("col-xs-12", "fwd","Fast Foreward", "fwd");
+  website = website + endDiv;
+  website = website + rowDiv;
+  website = website + generateButton("col-xs-12", "rwd","Rewind", "rwd");
   website = website + endDiv;
   // ------------------------- Volume Controls --------------------------
   website = website + rowDiv;
@@ -65,24 +77,24 @@ void handleRoot() {
   website = website + rowDiv;
   website = website + generateButton("col-xs-12", "down","Vol Down", "down");
   website = website + endDiv;
-  // ------------------------- Satelite Controls --------------------------
-  // Not working well
-//  website = website + rowDiv;
-//  website = website + generateButton("col-xs-offset-4 col-xs-4", "satup","Sat Up", "satup");
-//  website = website + generateButton("col-xs-4", "satblue","Sat Blue", "satblue");
-//  website = website + endDiv;
-//  website = website + rowDiv;
-//  website = website + generateButton("col-xs-4", "satexit","Sat Exit", "satexit");
-//  website = website + generateButton("col-xs-4", "satdown","Sat Down", "satdown");
-//  website = website + generateButton("col-xs-4", "satok","Sat Ok", "satok");
-//  website = website + endDiv;
-  // ------------------------- Chromecast --------------------------
+  // ------------------------- Favorites Controls --------------------------
   website = website + rowDiv;
-  website = website + generateButton("col-xs-12", "chromecast","Chromecast", "chromecast");
+  website = website + generateButton("col-xs-4", "1","1", "1");
+  website = website + generateButton("col-xs-4", "2","2", "2");
+  website = website + generateButton("col-xs-4", "3","3", "3");
   website = website + endDiv;
   website = website + rowDiv;
-  website = website + generateButton("col-xs-12", "togglesource","TV Source", "togglesource");
+  website = website + generateButton("col-xs-4", "4","4", "4");
+  website = website + generateButton("col-xs-4", "5","5", "5");
+  website = website + generateButton("col-xs-4", "6","6", "6");
   website = website + endDiv;
+  website = website + rowDiv;
+  website = website + generateButton("col-xs-4", "7","7", "7");
+  website = website + generateButton("col-xs-4", "8","8", "8");
+  website = website + generateButton("col-xs-4", "9","9", "9");
+  website = website + endDiv;
+  // ------------------------- End Bits --------------------------
+  
   website = website + endDiv;
   website = website + "    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js\"></script>\n";
   website = website + "    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>\n";
@@ -115,7 +127,7 @@ void setup(void){
   irsend.begin();
   pinMode(led, OUTPUT);
   digitalWrite(led, 1);
-  Serial.begin(115200);
+  Serial.begin(9600);
   WiFi.begin(ssid, password);
   Serial.println("");
 
@@ -130,126 +142,158 @@ void setup(void){
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("esp8266")) {
+  if (MDNS.begin("BTRadio")) {
     Serial.println("MDNS Responder Started");
   }
 
   server.on("/", handleRoot);
 
   server.on("/down", [](){
-    Serial.println("Sorround Sound Down");
-    irsend.sendNEC(0x4BB6C03F, 32);
+    Serial.println("Volume Down");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x16};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
     server.send(200, "text/plain", "Volume Down");
   });
 
   server.on("/up", [](){
-    Serial.println("Surround Sound Up");
-    irsend.sendNEC(0x4BB640BF, 32);
+    Serial.println("Volume Up");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x19};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
     server.send(200, "text/plain", "Volume Up");
   });
 
-  server.on("/sspower", [](){
-    Serial.println("Surround Sound power");
-    irsend.sendNEC(0x4B36D32C, 32);
-    server.send(200, "text/plain", "Surround Sound Power");
+  server.on("/stop", [](){
+    Serial.println("Stop");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x46};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "Stop");
   });
 
-  server.on("/sschannel1", [](){
-    Serial.println("Surround Sound Channel 1");
-    irsend.sendNEC(0x4B3631CE, 32);
-    server.send(200, "text/plain", "Surround Sound Channel 1");
+  server.on("/mute", [](){
+    Serial.println("Mute");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x47};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "Mute");
   });
 
-  server.on("/sschannel2", [](){
-    Serial.println("Surround Sound Channel 2");
-    irsend.sendNEC(0x4BB6F00F, 32);
-    server.send(200, "text/plain", "Surround Sound Channel 2");
+  server.on("/back", [](){
+    Serial.println("Back");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x40};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "Back");
   });
 
-  server.on("/sschannel3", [](){
-    Serial.println("Surround Sound Channel 3");
-    irsend.sendNEC(0x4BB6708F, 32);
-    server.send(200, "text/plain", "Surround Sound Channel 3");
+  server.on("/eq", [](){
+    Serial.println("EQ");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x43};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "EQ");
   });
 
-  server.on("/sschannel4", [](){
-    Serial.println("Surround Sound Channel 4");
-    irsend.sendNEC(0x4BB6B04F, 32);
-    server.send(200, "text/plain", "Surround Sound Channel 4");
+  server.on("/rwd", [](){
+    Serial.println("Rewind");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x07};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "Rewind");
   });
 
-  server.on("/sstvsound", [](){
-    Serial.println("Surround Sound sstvsound");
-    irsend.sendNEC(0x4BB6906F, 32);
-    server.send(200, "text/plain", "Surround Sound sstvsound");
+  server.on("/fwd", [](){
+    Serial.println("Fast Foreward");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x15};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "Fast Foreward");
   });
 
-  server.on("/tvpower", [](){
-    Serial.println("TV power");
-    irsend.sendNEC(0x20DF10EF, 32);
-    server.send(200, "text/plain", "TV Power");
+  server.on("/power", [](){
+    Serial.println("Power");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x45};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "Power");
   });
 
-  server.on("/tvsource", [](){
-    Serial.println("TV Source");
-    irsend.sendNEC(0x20DFD02F, 32);
-    server.send(200, "text/plain", "TV Source");
+  server.on("/play", [](){
+    Serial.println("Play");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x09};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "Play");
   });
 
-  server.on("/togglesource", [](){
-    Serial.println("TV Source");
-    irsend.sendNEC(0x20DFD02F, 32);
-    delay(DELAY_BETWEEN_COMMANDS);
-    irsend.sendNEC(0x20DFD02F, 32);
-    server.send(200, "text/plain", "TV Source");
+  server.on("/0", [](){
+    Serial.println("0");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x0D};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "0");
   });
 
-  server.on("/satpower", [](){
-    Serial.println("Sat Power");
-    irsend.sendNEC(0xA25D7887, 32);
-    server.send(200, "text/plain", "Sat Power");
+  server.on("/mode", [](){
+    Serial.println("Mode");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x44};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "Mode");
   });
 
-  server.on("/satok", [](){
-    Serial.println("Sat Ok");
-    irsend.sendNEC(0xA25DFA05, 32);
-    server.send(200, "text/plain", "Sat OK");
+  server.on("/1", [](){
+    Serial.println("1");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x0C};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "1");
   });
 
-  server.on("/satexit", [](){
-    Serial.println("Sat Exit");
-    irsend.sendNEC(0xA25D06F9, 32);
-    server.send(200, "text/plain", "Sat Exit");
+  server.on("/2", [](){
+    Serial.println("2");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x18};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "2");
   });
 
-  server.on("/satup", [](){
-    Serial.println("Sat UP");
-    irsend.sendNEC(0xA25DC03F, 32);
-    server.send(200, "text/plain", "Sat UP");
+  server.on("/3", [](){
+    Serial.println("3");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x5E};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "3");
   });
 
-  server.on("/satdown", [](){
-    Serial.println("Sat Down");
-    irsend.sendNEC(0xA25D7A85, 32);
-    server.send(200, "text/plain", "Sat Down");
+  server.on("/4", [](){
+    Serial.println("4");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x08};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "4");
   });
 
-  server.on("/satblue", [](){
-    Serial.println("Sat Blue");
-    irsend.sendNEC(0xA25D52AD, 32);
-    server.send(200, "text/plain", "Sat Blue");
+  server.on("/5", [](){
+    Serial.println("5");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x1C};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "5");
   });
 
-  server.on("/chromecast", [](){
-    Serial.println("Chromecast");
-    irsend.sendNEC(0x20DFD02F, 32);
-    delay(DELAY_BETWEEN_COMMANDS);
-    irsend.sendNEC(0x20DFD02F, 32);
-    delay(DELAY_BETWEEN_COMMANDS);
-    irsend.sendNEC(0x4BB6906F, 32);
-    server.send(200, "text/plain", "Chromecast");
+  server.on("/6", [](){
+    Serial.println("6");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x18};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "6");
   });
 
+  server.on("/7", [](){
+    Serial.println("7");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x5E};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "7");
+  });
+
+  server.on("/8", [](){
+    Serial.println("8");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x08};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "8");
+  });
+
+  server.on("/9", [](){
+    Serial.println("9");
+    uint8_t my_serial_bytes[5]={0xA1, 0xF1, 0x00, 0xFF, 0x1C};
+    Serial.write(my_serial_bytes,sizeof(my_serial_bytes));
+    server.send(200, "text/plain", "9");
+  });
 
   server.onNotFound(handleNotFound);
 
